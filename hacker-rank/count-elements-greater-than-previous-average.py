f8b6c0546177b6b06053babb8d9d346f68b94795
USER <sephardjetri20@gmail.com>
2025-12-22 18:35:06 +0700

    lupa menambahkan perhitungan avg-nya

--- hacker-rank/count-elements-greater-than-previous-average.py
+++ hacker-rank/count-elements-greater-than-previous-average.py
@@ -2,11 +2,12 @@ def countResponseTimeRegressions(responseTimes):
     if len(responseTimes) <= 1:
         return 0
     
-    avg = responseTimes[0]
+    current = responseTimes[0]
     count = 0
     for i in range(1, len(responseTimes)):
+        avg = current / i
         if responseTimes[i] > avg:
             count += 1
-            avg += responseTimes[i] / i
+        current += responseTimes[i]
             
     return count
